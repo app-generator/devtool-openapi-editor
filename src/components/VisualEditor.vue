b5a35f547a9ea8c3d17c5bd6135d91404a3b144d
deroude <rvgabriel@yahoo.com>
2022-04-28 00:35:08 +0300

    added params

--- src/components/VisualEditor.vue
+++ src/components/VisualEditor.vue
@@ -125,7 +125,7 @@
                       onEdit();
                     "
                     @cancel="editValue = ''"
-                    @edit="editValue = field.name"
+                    @edit="editValue = field.type"
                   >
                     <template v-slot:label> Type </template>
                     <template v-slot:value>{{ field.type }}</template>
@@ -249,12 +249,86 @@
                       ></v-text-field>
                     </template>
                   </Tree>
+                  <Tree showAdd @add="addParam(op)">
+                    <template v-slot:label> Parameters </template>
+                    <template v-slot:children>
+                      <Tree
+                        v-for="param of op.params"
+                        :key="param.name"
+                        showDelete
+                        @delete="deleteParam(op, param.name)"
+                      >
+                        <template v-slot:label> {{ param.name }}</template>
+                        <template v-slot:children>
+                          <Tree
+                            showEdit
+                            @save="
+                              param.name = editValue;
+                              editValue = '';
+                              onEdit();
+                            "
+                            @cancel="editValue = ''"
+                            @edit="editValue = param.name"
+                          >
+                            <template v-slot:label> Name </template>
+                            <template v-slot:value>{{ param.name }}</template>
+                            <template v-slot:editor>
+                              <v-text-field
+                                v-model="editValue"
+                                density="compact"
+                                variant="outlined"
+                              ></v-text-field>
+                            </template>
+                          </Tree>
+                          <Tree
+                            showEdit
+                            @save="
+                              param.in = editValue;
+                              editValue = '';
+                              onEdit();
+                            "
+                            @cancel="editValue = ''"
+                            @edit="editValue = param.in"
+                          >
+                            <template v-slot:label> In </template>
+                            <template v-slot:value>{{ param.in }}</template>
+                            <template v-slot:editor>
+                              <v-select
+                                v-model="editValue"
+                                density="compact"
+                                variant="outlined"
+                                :items="paramInOptions"
+                              ></v-select>
+                            </template>
+                          </Tree>
+                          <Tree
+                            showEdit
+                            @save="
+                              param.type = editValue;
+                              editValue = '';
+                              onEdit();
+                            "
+                            @cancel="editValue = ''"
+                            @edit="editValue = param.type"
+                          >
+                            <template v-slot:label> Type </template>
+                            <template v-slot:value>{{ param.type }}</template>
+                            <template v-slot:editor>
+                              <v-select
+                                v-model="editValue"
+                                density="compact"
+                                variant="outlined"
+                                :items="fieldTypes"
+                              ></v-select>
+                            </template>
+                          </Tree>
+                        </template>
+                      </Tree>
+                    </template>
+                  </Tree>
                 </template>
-              </Tree>
-            </template>
-          </Tree>
-        </template>
-      </Tree> </template
+              </Tree> </template
+          ></Tree> </template></Tree></template
   ></Tree>
 </template>
 
@@ -383,6 +457,10 @@ export default class VisualEditor extends Vue {
 
   get httpCodes(): Named[] {
     return HttpCode;
+  }
+
+  get paramInOptions(): string[] {
+    return ["query", "header", "path", "cookie"];
   }
 }
 </script>
